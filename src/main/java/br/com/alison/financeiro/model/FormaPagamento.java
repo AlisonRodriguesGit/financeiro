09cbd8b4e15d123113e74010a9a607c3b8727e3d
Alison Rodrigues <alison.aro@gmail.com>
2021-04-13 09:13:57 -0300

    criação do projeto

---
+++ src/main/java/br/com/alison/financeiro/model/FormaPagamento.java
@@ -0,0 +1,20 @@
+package br.com.alison.financeiro.model;
+
+public class FormaPagamento {
+    public String descricao;
+
+    public FormaPagamento(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public FormaPagamento(){
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
